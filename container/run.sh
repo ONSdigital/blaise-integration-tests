be74c69687f875ea7b2d2aa7424c19ff619bdba3
ed <anax@hotmail.co.uk>
2020-09-07 09:04:52 +0000

    run container script

---
+++ container/run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -u
+
+docker run \
+  -it \
+  --rm \
+  --name bha \
+  -v $(pwd):/data \
+  -e BLAISE_USERNAME=blaise \
+  -e BLAISE_PASSWORD=bKaNalC6FSxpVGsW \
+  -e BLAISE_HOSTNAME=dev-ed-84-client-tel.social-surveys.gcp.onsdigital.uk \
+  -e BLAISE_PORT=8031 \
+  -e BSM_USERNAME=admin \
+  -e BSM_PASSWORD=admin \
+  -e BSM_HOSTNAME=dev-ed-84-bsm.social-surveys.gcp.onsdigital.uk \
+  -e LOG_LEVEL=DEBUG \
+  behave \
+  /bin/bash -c "cd /data; behave --no-color --no-capture --no-logcapture"
+
+#  /bin/bash -c "cd /data; behave"
