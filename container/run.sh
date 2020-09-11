c8b05d96404c86bd5749e08e64665b4d22072baf
ed <anax@hotmail.co.uk>
2020-09-11 08:47:47 +0000

    mothballed

--- container/run.sh
+++ container/run.sh
@@ -14,6 +14,7 @@ docker run \
   -e BSM_HOSTNAME=dev-ed-84-bsm.social-surveys.gcp.onsdigital.uk \
   -e LOG_LEVEL=DEBUG \
   behave \
-  /bin/bash -c "cd /data; behave --no-color --no-capture --no-logcapture"
+  /bin/bash -c "cd /data; behave -n 'add a survey'"
 
+#  /bin/bash -c "cd /data; behave --no-color --no-capture --no-logcapture"
 #  /bin/bash -c "cd /data; behave"
