f729632f9d4f750e046a0e336af9a8a191060a01
jamie kerr <jamiestephenkerr@gmail.com>
2020-09-28 17:52:52 +0100

    Added project for testing blaise api

---
+++ Blaise/Blaise.Api.Tests.Behaviour/StepArgumentTransformations/CaseStepArgumentTransformations.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using StatNeth.Blaise.Shared.Collections;
+using TechTalk.SpecFlow;
+
+namespace Blaise.Api.Tests.Behaviour.StepArgumentTransformations
+{
+    [Binding]
+    public class ProcessNisraCasesStepArgumentTransformations
+    {
+        [StepArgumentTransformation]
+        public Dictionary<string, string> TransformFieldDataTableIntoListOfCaseModels(Table table)
+        {
+            return table.Rows.ToDictionary(r => r[0], r => r[1]);
+        }
+
+        [StepArgumentTransformation]
+        public IEnumerable<string> TransformFieldDataTableIntoListOfStrings(Table table)
+        {
+            return table.Rows.ToList(r => r[0]);
+        }
+    }
+}
