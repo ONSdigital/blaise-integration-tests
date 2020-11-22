4905b6cbb8f650f1f5f5960e606081c32fa9a5ec
Nik Webb <nik.webb@ons.gov.uk>
2020-11-22 15:15:15 +0000

    Added test to add cases to an instrument Added test to add a user

---
+++ Blaise/Blaise.Smoke.Tests/Models/CaseModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blaise.Smoke.Tests.Models
+{
+    public class CaseModel : IEquatable<CaseModel>
+    {
+        public string PrimaryKey { get; set; }
+        public string Outcome { get; set; }
+        public string TelNo { get; set; }
+
+        public bool Equals(CaseModel other)
+        {
+            if (other is null)
+                return false;
+
+            return this.PrimaryKey == other.PrimaryKey && this.Outcome == other.Outcome && this.TelNo == other.TelNo;
+        }
+
+        public Dictionary<string, string> ConvertToDictionary()
+        {
+            var dic = new Dictionary<string, string>();
+            dic.Add("serial_number", this.PrimaryKey);
+            dic.Add("QHAdmin.HOut", this.Outcome);
+            dic.Add("QDataBag.TelNo", this.TelNo);
+            return dic;
+        }
+
+        public override bool Equals(object obj) => Equals(obj as CaseModel);
+        public override int GetHashCode() => (PrimaryKey, Outcome, TelNo).GetHashCode();
+
+    }
+}
---
+++ Blaise/Blaise.Smoke.Tests/StepArguement/SmokeTestStepsTransformation.cs
@@ -0,0 +1,21 @@
+﻿using Blaise.Smoke.Tests.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Assist;
+
+namespace Blaise.Smoke.Tests.StepArguement
+{
+    [Binding]
+    public class SmokeTestStepsTransformation
+    {
+        [StepArgumentTransformation]
+        public IEnumerable<CaseModel> TransformCaseTableIntoListCaseModel(Table table)
+        {
+            return table.CreateSet<CaseModel>();
+        }
+    }
+}
