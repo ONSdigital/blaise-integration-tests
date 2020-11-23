ea1e066b83050079dbadc0a4f43f1cee0e82c54d
Nicholas <nik.webb@ons.gov.uk>
2020-11-23 08:23:20 +0000

    Added Pages for Selenium Test

---
+++ Blaise/Blaise.Smoke.Tests/Pages/DayBatchPage.cs
@@ -0,0 +1,43 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Blaise.Smoke.Tests.Pages
+{
+    class DayBatchPage
+    {
+        private IWebDriver _driver;
+
+        private By dayBatchCreateButton = By.Id("btnCreateDaybatch");
+        private By createButton = By.XPath("//input[@value='Create']");
+        private By numberOfEnteries = By.XPath("//div[contains(text(), 'Showing')]");
+
+        public DayBatchPage(IWebDriver driver)
+        {
+            this._driver = driver;
+        }
+
+        public string CreateDayBatch()
+        {
+            _driver.FindElement(dayBatchCreateButton).Click();
+            Thread.Sleep(2000);
+            _driver.FindElement(createButton).Click();
+            Thread.Sleep(2000);
+            return _driver.FindElement(numberOfEnteries).Text;
+        }
+
+        public void GoToPage()
+        {
+            _driver.Navigate().GoToUrl("");
+        }
+
+        public string GetTitle()
+        {
+            return _driver.Title;
+        }
+    }
+}
---
+++ Blaise/Blaise.Smoke.Tests/Pages/SurveyPage.cs
@@ -0,0 +1,29 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blaise.Smoke.Tests.Pages
+{
+    class SurveyPage
+    {
+        private IWebDriver _driver;
+
+        public SurveyPage(IWebDriver driver)
+        {
+            this._driver = driver;
+            if (_driver.Title != ("Surveys - CATI Dashboard"))
+            {
+                throw new Exception("This is not the Survey Page of logged in user," +
+                      " current page is: " + _driver.Url);
+            }
+        }
+
+        public string GetTitle()
+        {
+            return _driver.Title;
+        }
+    }
+}
