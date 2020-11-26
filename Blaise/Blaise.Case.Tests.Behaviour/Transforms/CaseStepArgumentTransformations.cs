a86ee229875638b88d35e317a1d81659884a953c
Jamie Kerr <jamiestephenkerr@gmail.com>
2020-11-26 13:53:34 +0000

    Completed scenario 1

---
+++ Blaise/Blaise.Case.Tests.Behaviour/Transforms/CaseStepArgumentTransformations.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Blaise.Tests.Models;
+using Blaise.Tests.Models.Case;
+using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Assist;
+
+namespace Blaise.Case.Tests.Behaviour.Transforms
+{
+    [Binding]
+    public class CaseStepArgumentTransformations
+    {
+        [StepArgumentTransformation]
+        public IEnumerable<CaseModel> TransformCaseTableIntoListCaseModel(Table table)
+        {
+            return table.CreateSet<CaseModel>();
+        }
+    }
+}
