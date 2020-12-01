c02dbd40a327cf0bf2f9df3e5552c9771e202f65
Nik Webb <nik.webb@ons.gov.uk>
2020-12-01 17:40:54 +0000

    Renamed Cati to Cati.Management Added in Cati Interview files for the scenario

---
+++ Blaise/Blaise.Cati.Interview.Tests.Behaviour/Steps/AccessCaseSteps.cs
@@ -0,0 +1,39 @@
+﻿using Blaise.Tests.Helpers.Case;
+using Blaise.Tests.Helpers.Cati;
+using Blaise.Tests.Helpers.Instrument;
+using NUnit.Framework;
+using TechTalk.SpecFlow;
+
+namespace Blaise.Cati.Interview.Tests.Behaviour.Steps
+{
+    [Binding]
+    public sealed class AccessCaseSteps
+    {
+        [BeforeFeature("interview")]
+        public static void InitializeFeature()
+        {
+            CatiInterviewHelper.GetInstance().CreateInterviewUser();
+        }
+
+        [When(@"I log on to the Interviewing Portal as an interviewer")]
+        public void WhenIAccessTheCaseFromTheCaseInterviewingScreen()
+        {
+            CatiInterviewHelper.GetInstance().LogIntoInterviewPortal();
+        }
+
+        [Then(@"I am able to capture the respondents data for case '(.*)'")]
+        public void ThenIAmAbleToCaptureTheRespondentsDataForCase(string caseId)
+        {
+            var caseIdText = CatiInterviewHelper.GetInstance().GetCaseIdText();
+            Assert.AreEqual($"Case: {caseId}", caseIdText);
+        }
+
+        [AfterFeature("interview")]
+        public static void CleanUpFeature()
+        {
+            CatiInterviewHelper.GetInstance().DeleteInterviewUser();
+            //CaseHelper.GetInstance().DeleteCases();
+            //InstrumentHelper.GetInstance().UninstallSurvey();
+        }
+    }
+}
