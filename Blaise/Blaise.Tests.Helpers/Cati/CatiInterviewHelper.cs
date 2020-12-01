ce9e881e4e69f40dd97c917bde5f1642453a6c89
Nik Webb <nik.webb@ons.gov.uk>
2020-12-01 17:44:18 +0000

    Added steps to complete scenario 1 Updated app config files Implemented tags of feature steps Renamed helpers to match solutions Added a user model

---
+++ Blaise/Blaise.Tests.Helpers/Cati/CatiInterviewHelper.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using Blaise.Tests.Helpers.Cati.Pages;
+using Blaise.Tests.Helpers.Configuration;
+using Blaise.Tests.Helpers.User;
+using Blaise.Tests.Models.User;
+using OpenQA.Selenium.Chrome;
+
+namespace Blaise.Tests.Helpers.Cati
+{
+    public class CatiInterviewHelper
+    {
+        private static CatiInterviewHelper _currentInstance;
+        private ChromeDriver _driver;
+
+        public static CatiInterviewHelper GetInstance()
+        {
+            return _currentInstance ?? (_currentInstance = new CatiInterviewHelper());
+        }
+
+        public CatiInterviewHelper()
+        {
+            _driver = new ChromeDriver(CatiConfigurationHelper.ChromeDriver);
+        }
+
+        public void LogIntoInterviewPortal()
+        {
+            var interviewLoginPage = new InterviewLoginPage(_driver);
+            interviewLoginPage.LoadPage();
+            interviewLoginPage.LogIntoInterviewPortal(CatiConfigurationHelper.CatiInterviewUsername, CatiConfigurationHelper.CatiInterviewPassword);
+        }
+
+        public void CreateInterviewUser()
+        {
+            var interviewUser = new UserModel
+            {
+                UserName = CatiConfigurationHelper.CatiInterviewUsername,
+                Password = CatiConfigurationHelper.CatiInterviewPassword,
+                Role = CatiConfigurationHelper.InterviewRole,
+                ServerParks = new List<string>{BlaiseConfigurationHelper.ServerParkName},
+                DefaultServerPark = BlaiseConfigurationHelper.ServerParkName
+            };
+
+            UserHelper.GetInstance().CreateUser(interviewUser);
+        }
+
+        public string GetCaseIdText()
+        {
+            var interviewPage = new InterviewPage(_driver);
+            return interviewPage.GetCaseIdText();
+        }
+
+        public void DeleteInterviewUser()
+        {
+            UserHelper.GetInstance().RemoveUser(CatiConfigurationHelper.CatiInterviewUsername);
+        }
+    }
+}
---
+++ Blaise/Blaise.Tests.Helpers/Cati/Pages/InterviewLoginPage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Blaise.Tests.Helpers.Configuration;
+using OpenQA.Selenium;
+
+namespace Blaise.Tests.Helpers.Cati.Pages
+{
+    public class InterviewLoginPage : BasePage
+    {
+        private readonly string _usernameTextboxName = "name";
+        private readonly string _passwordTextboxName = "password";
+        private readonly string _submitButtonPath = "//input[@type='submit']";
+
+        public InterviewLoginPage(IWebDriver driver) : base(driver, CatiConfigurationHelper.InterviewUrl)
+        {
+        }
+
+        public void LogIntoInterviewPortal(string username, string password)
+        {
+            PopulateTextboxByName(_usernameTextboxName, username);
+            PopulateTextboxByName(_passwordTextboxName, password);
+            ClickButtonByXPath(_submitButtonPath);
+        }
+    }
+}
