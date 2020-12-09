3b7ae850634430549fa3076dd93f4e3eabf130fa
Nik Webb <nik.webb@ons.gov.uk>
2020-12-09 15:32:30 +0000

    WIP

--- Blaise/Blaise.Tests.Helpers/RestApi/RestApiHelper.cs
+++ Blaise/Blaise.Tests.Helpers/RestApi/RestApiHelper.cs
@@ -1,12 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
+using Blaise.Tests.Helpers.Configuration;
+using Newtonsoft.Json;
+using StatNeth.Blaise.API.ServerManager;
 
 namespace Blaise.Tests.Helpers.RestApi
 {
     public class RestApiHelper
     {
+        private static HttpClient _httpClient;
+
+        public RestApiHelper()
+        {
+            _httpClient = new HttpClient {BaseAddress = new Uri(RestApiConfigurationHelper.BaseUrl)};
+            _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        public static async Task<List<Questionnaire>> GetAllActiveQuestionnaires()
+        {
+            return await GetASync<Questionnaire>("questionnaire").ConfigureAwait(false);
+        }
+
+        private static async Task<T> GetASync<T>(string url)
+        {
+            var response = await _httpClient.GetAsync(url);
+
+            if (response.StatusCode != HttpStatusCode.OK) return default;
+
+            var responseAsJson = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<T>(responseAsJson);
+        }
     }
 }
