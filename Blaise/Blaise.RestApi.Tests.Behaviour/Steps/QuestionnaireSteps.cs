cbf356a5bb53e46562fa0b9449e9512fa40958f5
Nik Webb <nik.webb@ons.gov.uk>
2020-12-09 13:54:05 +0000

    Added Initial RestApi Test project to the solution

---
+++ Blaise/Blaise.RestApi.Tests.Behaviour/Steps/QuestionnaireSteps.cs
@@ -0,0 +1,15 @@
+﻿using TechTalk.SpecFlow;
+
+namespace Blaise.RestApi.Tests.Behaviour.Steps
+{
+    [Binding]
+    public sealed class QuestionnaireSteps
+    {
+        [When(@"the API is queried to return all active questionnaires")]
+        public void WhenTheAPIIsQueriedToReturnAllActiveQuestionnaires()
+        {
+
+        }
+
+    }
+}
