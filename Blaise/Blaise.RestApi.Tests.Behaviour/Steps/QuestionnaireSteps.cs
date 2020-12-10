3598b9dbaaa75a7397ef32f114e7786be762b618
Nik Webb <nik.webb@ons.gov.uk>
2020-12-10 10:17:43 +0000

    Added framework for scenario 1

--- Blaise/Blaise.RestApi.Tests.Behaviour/Steps/QuestionnaireSteps.cs
+++ Blaise/Blaise.RestApi.Tests.Behaviour/Steps/QuestionnaireSteps.cs
@@ -1,4 +1,7 @@
-﻿using Blaise.Tests.Helpers.RestApi;
+﻿using System.Collections.Generic;
+using Blaise.Tests.Helpers.RestApi;
+using Blaise.Tests.Models;
+using NUnit.Framework;
 using TechTalk.SpecFlow;
 
 namespace Blaise.RestApi.Tests.Behaviour.Steps
@@ -6,11 +9,26 @@ namespace Blaise.RestApi.Tests.Behaviour.Steps
     [Binding]
     public sealed class QuestionnaireSteps
     {
+        private readonly ScenarioContext _scenarioContext;
+        private const string ApiResponse = "ApiResponse";
+
+        public QuestionnaireSteps(ScenarioContext scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+        }
+
         [When(@"the API is queried to return all active questionnaires")]
         public void WhenTheApiIsQueriedToReturnAllActiveQuestionnaires()
         {
-           var n = RestApiHelper.GetAllActiveSurveys<Questionnaire>("Questionnaires");
+           var listOfActiveQuestionnaires = RestApiHelper.GetAllActiveQuestionnaires();
+           _scenarioContext.Set(listOfActiveQuestionnaires, ApiResponse);
         }
 
+        [Then(@"details of questionnaire a is returned")]
+        public void ThenDetailsOfQuestionnaireAIsReturned()
+        {
+            var listOfActiveQuestionnaires = _scenarioContext.Get<List<Questionnaire>>(ApiResponse);
+            Assert.AreEqual(1, listOfActiveQuestionnaires.Count);
+        }
     }
 }
