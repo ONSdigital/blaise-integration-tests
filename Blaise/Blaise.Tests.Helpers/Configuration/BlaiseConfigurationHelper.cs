fb21b4fb459d6d8abb12371926778e5eaec95dc0
Nik Webb <nik.webb@ons.gov.uk>
2020-12-10 09:08:09 +0000

    Added Configuration extension and error messages for misconfiguration

--- Blaise/Blaise.Tests.Helpers/Configuration/BlaiseConfigurationHelper.cs
+++ Blaise/Blaise.Tests.Helpers/Configuration/BlaiseConfigurationHelper.cs
@@ -1,37 +1,28 @@
 ﻿using System;
 using System.Configuration;
 using Blaise.Nuget.Api.Contracts.Models;
+using Blaise.Tests.Helpers.Extensions;
 
 namespace Blaise.Tests.Helpers.Configuration
 {
     public static class BlaiseConfigurationHelper
     {
-        public static string ServerParkName => GetVariable("ServerParkName");
-        public static string InstrumentPath => GetVariable("InstrumentPath");
-        public static string InstrumentName => GetVariable("InstrumentName");
+        public static string ServerParkName => ConfigurationExtensions.GetVariable("ServerParkName");
+        public static string InstrumentPath => ConfigurationExtensions.GetVariable("InstrumentPath");
+        public static string InstrumentName => ConfigurationExtensions.GetVariable("InstrumentName");
         public static string InstrumentPackage => $"{InstrumentPath}//{InstrumentName}.zip";
 
         public static ConnectionModel BuildConnectionModel()
         {
             return new ConnectionModel{
-                ServerName = GetVariable("BlaiseServerHostName"),
-                UserName = GetVariable("BlaiseServerUserName"),
-                Password = GetVariable("BlaiseServerPassword"),
-                Binding = GetVariable("BlaiseServerBinding"),
-                Port = GetIntVariable("BlaiseConnectionPort"),
-                RemotePort = GetIntVariable("BlaiseRemoteConnectionPort"),
-                ConnectionExpiresInMinutes = GetIntVariable("ConnectionExpiresInMinutes")
+                ServerName = ConfigurationExtensions.GetVariable("BlaiseServerHostName"),
+                UserName = ConfigurationExtensions.GetVariable("BlaiseServerUserName"),
+                Password = ConfigurationExtensions.GetVariable("BlaiseServerPassword"),
+                Binding = ConfigurationExtensions.GetVariable("BlaiseServerBinding"),
+                Port = ConfigurationExtensions.GetIntVariable("BlaiseConnectionPort"),
+                RemotePort = ConfigurationExtensions.GetIntVariable("BlaiseRemoteConnectionPort"),
+                ConnectionExpiresInMinutes = ConfigurationExtensions.GetIntVariable("ConnectionExpiresInMinutes")
             };
-        }
-
-        private static string GetVariable(string variableName)
-        {
-            return ConfigurationManager.AppSettings[variableName];
-        }
-
-        private static int GetIntVariable(string variableName)
-        {
-            return Convert.ToInt32(ConfigurationManager.AppSettings[variableName]);
         }
     }
 }
