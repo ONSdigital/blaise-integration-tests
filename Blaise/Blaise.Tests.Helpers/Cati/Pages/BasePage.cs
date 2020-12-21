255f5770485ecd9dc75fd7ef86e63e042d79aadd
Nik Webb <nik.webb@ons.gov.uk>
2020-12-21 14:24:02 +0000

    Tidy up using statements

--- Blaise/Blaise.Tests.Helpers/Cati/Pages/BasePage.cs
+++ Blaise/Blaise.Tests.Helpers/Cati/Pages/BasePage.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using Blaise.Tests.Helpers.Browser;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Support.UI;
