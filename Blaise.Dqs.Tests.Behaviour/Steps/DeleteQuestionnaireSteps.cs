f73549e6ae9f988df70930bc4371a035f5895fa7
Nik Webb <nik.webb@ons.gov.uk>
2021-01-20 12:28:07 +0000

    Added Deletion scenarios

---
+++ Blaise.Dqs.Tests.Behaviour/Steps/DeleteQuestionnaireSteps.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Blaise.Tests.Helpers.Case;
+using Blaise.Tests.Helpers.Cati;
+using Blaise.Tests.Helpers.Configuration;
+using Blaise.Tests.Helpers.Dqs;
+using Blaise.Tests.Helpers.Instrument;
+using Blaise.Tests.Helpers.Tobi;
+using NUnit.Framework;
+using TechTalk.SpecFlow;
+
+namespace Blaise.Dqs.Tests.Behaviour.Steps
+{
+    [Binding]
+    public class DeleteQuestionnaireSteps
+    {
+        [Given(@"I have a questionnaire I want to delete")]
+        public void GivenIHaveTheNameOfAQuestionnaireIWantToDeleteAndThatSurveyIsLive()
+        {
+            InstrumentHelper.GetInstance().InstallInstrument();
+        }
+
+        [Given(@"that survey is live")]
+        public void GivenThatSurveyIsLive()
+        {
+            DayBatchHelper.GetInstance().SetSurveyDay(BlaiseConfigurationHelper.InstrumentName, DateTime.Today);
+        }
+
+        [Given(@"I select Delete on a questionnaire that is not live")]
+        public void GivenISelectDeleteOnAQuestionnaireThatIsNotLive()
+        {
+            InstrumentHelper.GetInstance().InstallInstrument();
+            DqsHelper.GetInstance().DeleteQuestionnaire(BlaiseConfigurationHelper.InstrumentName);
+        }
+
+        [When(@"I locate that questionnaire in the list")]
+        public void WhenILocateThatQuestionnaireInTheList()
+        {
+            DqsHelper.GetInstance().LoadDqsHomePage();
+            var questionnairesInTable = DqsHelper.GetInstance().GetQuestionnaireTableContents();
+            Assert.IsTrue(questionnairesInTable.Any(q => q == BlaiseConfigurationHelper.InstrumentName));
+        }
+
+        [When(@"I confirm that I want to proceed")]
+        public void WhenIConfirmThatIWantToProceed()
+        {
+            DqsHelper.GetInstance().ConfirmDeletionOfQuestionnaire();
+        }
+
+
+        [Then(@"I will not have the option to delete displayed")]
+        public void ThenIWillNotHaveTheOptionToDeleteDisplayed()
+        {
+            Assert.AreEqual("Questionnaire is live",DqsHelper.GetInstance().GetTextForDeletion());
+        }
+
+        [Then(@"the questionnaire is removed from Blaise")]
+        public void ThenTheQuestionnaireIsRemovedFromBlaise()
+        {
+            Assert.IsFalse(InstrumentHelper.GetInstance().SurveyExists(BlaiseConfigurationHelper.InstrumentName));
+        }
+
+
+    }
+}
