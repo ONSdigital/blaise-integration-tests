f021d2958ebc32c6a2b5a332286eee090bee4f19
Nik Webb <nik.webb@ons.gov.uk>
2021-01-25 08:39:41 +0000

    Lu 7996 (#15)
    * Added Deletion scenarios
    
    * Added cleanup steps
    
    * Updated App config names and added in clean up steps for Cati

--- Blaise.Tests.Helpers/Configuration/BlaiseConfigurationHelper.cs
+++ Blaise.Tests.Helpers/Configuration/BlaiseConfigurationHelper.cs
@@ -15,10 +15,10 @@ namespace Blaise.Tests.Helpers.Configuration
         public static ConnectionModel BuildConnectionModel()
         {
             return new ConnectionModel{
-                ServerName = ConfigurationExtensions.GetVariable("ENV_BLAISE_SERVER_HOST_NAME"),
+                ServerName = ConfigurationExtensions.GetVariable("ENV_BLAISE_EXTERNAL_SERVER_HOST_NAME"),
                 UserName = ConfigurationExtensions.GetVariable("ENV_BLAISE_ADMIN_USER"),
                 Password = ConfigurationExtensions.GetVariable("ENV_BLAISE_ADMIN_PASSWORD"),
-                Binding = ConfigurationExtensions.GetVariable("ENV_BLAISE_SERVER_BINDING"),
+                Binding = ConfigurationExtensions.GetVariable("ENV_BLAISE_EXTERNAL_SERVER_BINDING"),
                 Port = ConfigurationExtensions.GetIntVariable("ENV_BLAISE_CONNECTION_PORT"),
                 RemotePort = ConfigurationExtensions.GetIntVariable("ENV_BLAISE_REMOTE_CONNECTION_PORT"),
                 ConnectionExpiresInMinutes = ConfigurationExtensions.GetIntVariable("ENV_CONNECTION_EXPIRES_IN_MINUTES")
