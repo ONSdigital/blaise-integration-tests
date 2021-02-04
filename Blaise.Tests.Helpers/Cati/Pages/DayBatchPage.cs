ba49c4633a28bd6d5d4c2ed2be1a14fe86a9a19b
Nik Webb <nik.webb@ons.gov.uk>
2021-02-04 22:59:40 +0000

    Updated Cati tests to deal with multiple instruments

--- Blaise.Tests.Helpers/Cati/Pages/DayBatchPage.cs
+++ Blaise.Tests.Helpers/Cati/Pages/DayBatchPage.cs
@@ -1,6 +1,7 @@
 ﻿using Blaise.Tests.Helpers.Configuration;
 using Blaise.Tests.Helpers.Framework;
 using System;
+using System.Threading;
 
 namespace Blaise.Tests.Helpers.Cati.Pages
 {
@@ -9,10 +10,12 @@ namespace Blaise.Tests.Helpers.Cati.Pages
         private const string DayBatchCreateButtonId = "btnCreateDaybatch";
         private const string CreateButtonPath = "//input[@value='Create']";
         private const string NumberOfEntriesPath = "//div[contains(text(), 'Showing')]";
-        private const string ModifyEntryPath = "//*[@id='MVCGridTable_DaybatchGrid']/tbody/tr/td[17]/a";
+        private string DayBatchEntry = $"//table[@id='MVCGridTable_DaybatchGrid']//td[preceding-sibling::td='{BlaiseConfigurationHelper.InstrumentName}']";
+        private string ModifyEntryPath = $"//table[@id='MVCGridTable_DaybatchGrid']//td[preceding-sibling::td='{BlaiseConfigurationHelper.InstrumentName}']/a";
         private const string StartTimeId = "NewStartTimeAmPm";
         private const string EndTimeId = "NewEndTimeAmPm";
         private const string UpdateButtonPath = "//input[@value='Update']";
+        private const string InstrumentDropDownId = "InstrumentId";
 
         public DayBatchPage() : base(CatiConfigurationHelper.DayBatchUrl)
         {
@@ -21,12 +24,14 @@ namespace Blaise.Tests.Helpers.Cati.Pages
         public void CreateDayBatch()
         {
             ClickButtonById(DayBatchCreateButtonId);
+            SelectDropDownValueById(InstrumentDropDownId, BlaiseConfigurationHelper.InstrumentName);
+            Thread.Sleep(3000);
             ClickButtonByXPath(CreateButtonPath);
         }
 
         public string GetDaybatchEntriesText()
         {
-            return GetElementTextByPath(NumberOfEntriesPath);
+            return GetElementTextByPath(DayBatchEntry);
         }
 
         internal void ModifyDayBatchEntry()
