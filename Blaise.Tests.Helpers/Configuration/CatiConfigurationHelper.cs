0cae95fe592836fce7b1a9109c5ee05007a634c8
Nik Webb <nik.webb@ons.gov.uk>
2021-02-04 08:28:41 +0000

    Changed created username

--- Blaise.Tests.Helpers/Configuration/CatiConfigurationHelper.cs
+++ Blaise.Tests.Helpers/Configuration/CatiConfigurationHelper.cs
@@ -6,17 +6,15 @@ namespace Blaise.Tests.Helpers.Configuration
     public static class CatiConfigurationHelper
     {
         private static readonly Guid Password;
-        private static readonly string UserName;
 
         static CatiConfigurationHelper()
         {
-            UserName = "DSTTestUser" + RandomString(2);
             Password = Guid.NewGuid();
         }
 
         public static string CatiAdminUsername => BlaiseConfigurationHelper.BuildConnectionModel().UserName;
         public static string CatiAdminPassword => BlaiseConfigurationHelper.BuildConnectionModel().Password;
-        public static string CatiInterviewUsername => $"{UserName}";
+        public static string CatiInterviewUsername => "DSTTestUser";
         public static string CatiInterviewPassword => $"{Password}";
         public static string InterviewRole => $"DST";
         public static string CatiBaseUrl => $"{BlaiseConfigurationHelper.BuildConnectionModel().Binding}://{BlaiseConfigurationHelper.BuildConnectionModel().ServerName.Replace("client", "web")}";
@@ -26,14 +24,5 @@ namespace Blaise.Tests.Helpers.Configuration
         public static string SpecificationUrl => $"{CatiBaseUrl}/blaise/specification";
         public static string SurveyUrl => $"{CatiBaseUrl}/blaise/";
 
-        private static readonly Random random = new Random((int)DateTime.Now.Ticks);
-
-        private static string RandomString(int length)
-        {
-            const string pool = "abcdefghijklmnopqrstuvwxyz0123456789";
-            var chars = Enumerable.Range(0, length)
-                .Select(x => pool[random.Next(0, pool.Length)]);
-            return new string(chars.ToArray());
-        }
     }
 }
