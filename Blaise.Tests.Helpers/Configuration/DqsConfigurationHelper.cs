19d7b28de86ebecd9b605e12e879f86d67df3259
Nik Webb <nik.webb@ons.gov.uk>
2021-04-14 16:34:44 +0100

    Updated test configuration

--- Blaise.Tests.Helpers/Configuration/DqsConfigurationHelper.cs
+++ Blaise.Tests.Helpers/Configuration/DqsConfigurationHelper.cs
@@ -4,12 +4,12 @@ namespace Blaise.Tests.Helpers.Configuration
 {
     public static class DqsConfigurationHelper
     {
-        public static string DqsUrl => ConfigurationExtensions.GetVariable("ENV_DQS_URL");
-        public static string UploadUrl => $"{DqsUrl}/upload";
-        public static string UploadSummaryUrl => $"{DqsUrl}/UploadSummary";
+        public static string DqsUrl => $"{ConfigurationExtensions.GetVariable("ENV_DQS_URL")}/";
+        public static string UploadUrl => $"{DqsUrl}upload";
+        public static string UploadSummaryUrl => $"{DqsUrl}UploadSummary";
         public static string QuestionnaireExistsUrl => $"{UploadUrl}/survey-exists";
         public static string CannotOverwriteUrl  => $"{UploadUrl}/survey-live";
         public static string ConfirmOverwriteUrl => $"{UploadUrl}/survey-confirm";
-        public static string ConfirmDeleteUrl => $"{DqsUrl}/delete/";
+        public static string ConfirmDeleteUrl => $"{DqsUrl}delete/";
     }
 }
