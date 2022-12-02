8a46646b8974dd0413efa9e287abfe5e9e08fb4c
Jamie Kerr <60973239+jamiestephenkerr@users.noreply.github.com>
2022-12-02 10:56:40 +0000

    Pull through changes (#53)
    * Update xpath expressions to match new updates to design components
    
    * fml

--- Blaise.Tests.Helpers/DQS/Pages/UploadSummaryPage.cs
+++ Blaise.Tests.Helpers/DQS/Pages/UploadSummaryPage.cs
@@ -5,7 +5,7 @@ namespace Blaise.Tests.Helpers.Dqs.Pages
 {
     public class UploadSummaryPage : BasePage
     {
-        private const string SummaryDivPath = "//div[contains(@class, 'success panel')]";
+        private const string SummaryDivPath = "//div[contains(@class, 'success ons-panel')]";
 
         public UploadSummaryPage() : base(DqsConfigurationHelper.UploadSummaryUrl)
         {
