a4e8763bdca04fb81ae5d43dc482baf7b550a9a0
James Williams <james.williams@ons.gov.uk>
2023-08-14 15:18:31 +0100

    feat: add before test run hook to check erroneous q.naire

--- Blaise.Dqs.Tests.Behaviour/Steps/CommonHooks.cs
+++ Blaise.Dqs.Tests.Behaviour/Steps/CommonHooks.cs
@@ -1,19 +1,28 @@
 ﻿using Blaise.Tests.Helpers.Browser;
+using Blaise.Tests.Helpers.Instrument;
 using NUnit.Framework;
-using System;
+using System.IO;
 using TechTalk.SpecFlow;
 
 namespace Blaise.Tests.Helpers.ErrorHandler
 {
     [Binding]
-    public sealed class CommonErrorHookForSteps
+    public sealed class CommonHooks
     {
         private readonly ScenarioContext _scenarioContext;
 
-        public CommonErrorHookForSteps(ScenarioContext scenarioContext)
+        public CommonHooks(ScenarioContext scenarioContext)
         {
             _scenarioContext = scenarioContext;
         }
+
+
+        [BeforeTestRun]
+        public static void CheckForErroneousInstrument()
+        {
+            InstrumentHelper.GetInstance().CheckForErroneousInstrument("DST2304Z");
+        }
+
 
         [AfterStep]
         public void OnError()
@@ -21,7 +30,6 @@ namespace Blaise.Tests.Helpers.ErrorHandler
             if (_scenarioContext.TestError != null)
             {
                 BrowserHelper.OnError(TestContext.CurrentContext, _scenarioContext);
-                throw new Exception(_scenarioContext.TestError.Message);
             }
         }
     }
--- Blaise.Tests.Behaviour/Steps/CommonHooks.cs
+++ Blaise.Tests.Behaviour/Steps/CommonHooks.cs
@@ -1,26 +1,35 @@
 ﻿using Blaise.Tests.Helpers.Browser;
+using Blaise.Tests.Helpers.Instrument;
 using NUnit.Framework;
-using System.IO;
+using System;
 using TechTalk.SpecFlow;
 
 namespace Blaise.Tests.Helpers.ErrorHandler
 {
     [Binding]
-    public sealed class CommonErrorHookForSteps
+    public sealed class CommonHooks
     {
         private readonly ScenarioContext _scenarioContext;
 
-        public CommonErrorHookForSteps(ScenarioContext scenarioContext)
+        public CommonHooks(ScenarioContext scenarioContext)
         {
             _scenarioContext = scenarioContext;
         }
 
-        [AfterStep]
+        [BeforeTestRun]
+        public static void CheckForErroneousInstrument()
+        {
+            InstrumentHelper.GetInstance().CheckForErroneousInstrument("DST2304Z");
+        }
+
+
+    [AfterStep]
         public void OnError()
         {
             if (_scenarioContext.TestError != null)
             {
                 BrowserHelper.OnError(TestContext.CurrentContext, _scenarioContext);
+                throw new Exception(_scenarioContext.TestError.Message);
             }
         }
     }
