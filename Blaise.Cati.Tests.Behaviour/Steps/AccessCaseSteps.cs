09c484e83935aa3fe37ea0a054cb8e04410510f4
kristian4res <57638182+Enigma-cloud@users.noreply.github.com>
2023-08-15 15:03:19 +0100

    wip: test github pr green (tobi)

--- Blaise.Cati.Tests.Behaviour/Steps/AccessCaseSteps.cs
+++ Blaise.Cati.Tests.Behaviour/Steps/AccessCaseSteps.cs
@@ -46,7 +46,7 @@ namespace Blaise.Cati.Tests.Behaviour.Steps
         [Given(@"I log on to Cati as an interviewer")]
         public void GivenILogOnToCatiAsAnInterviewer()
         {
-            Console.WriteLine("Testing automated GitHub PR RED";
+            Console.WriteLine("Testing automated GitHub PR GREEN With TOBI");
             CatiManagementHelper.GetInstance().LogIntoCatiManagementPortalAsAnInterviewer();
         }
 
