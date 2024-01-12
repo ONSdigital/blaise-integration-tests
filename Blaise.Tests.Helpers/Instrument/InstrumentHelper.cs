a4d7dc3286dd5544b1954c6c2cb58c77a839acf8
elthorne <elinor.thorne@ons.gov.uk>
2024-01-12 16:36:55 +0000

    fix: increase timeout to 60 seconds
    refs: BLAIS5-3765

--- Blaise.Tests.Helpers/Instrument/InstrumentHelper.cs
+++ Blaise.Tests.Helpers/Instrument/InstrumentHelper.cs
@@ -104,7 +104,7 @@ namespace Blaise.Tests.Helpers.Instrument
             Console.WriteLine($"InstrumentHelper UninstallSurvey: Removing questionnaire {BlaiseConfigurationHelper.InstrumentName}");
             _blaiseQuestionnaireApi.UninstallQuestionnaire(BlaiseConfigurationHelper.InstrumentName, BlaiseConfigurationHelper.ServerParkName);
 
-            if (!SurveyHasUninstalled(BlaiseConfigurationHelper.InstrumentName, 10))
+            if (!SurveyHasUninstalled(BlaiseConfigurationHelper.InstrumentName, 60))
             {
                 CheckIfInstrumentIsErroneous(BlaiseConfigurationHelper.InstrumentName);
             }
