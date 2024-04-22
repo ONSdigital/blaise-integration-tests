65d2872fb882e22bb68ebf4ad3f133805d26e480
jamie kerr <jamiestephenkerr@gmail.com>
2024-04-22 13:49:46 +0100

    Removed some shenanigans

--- Blaise.Tests.Helpers/Framework/BasePage.cs
+++ Blaise.Tests.Helpers/Framework/BasePage.cs
@@ -22,13 +22,9 @@ namespace Blaise.Tests.Helpers.Framework
 
         protected void ClickButtonById(string buttonElementId)
         {
-            var button = BrowserHelper
+            BrowserHelper
                 .Wait($"Timed out in ClickButtonById(\"{buttonElementId}\")")
-                .Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.Id(buttonElementId)));
-
-            BrowserHelper.ScrollIntoView(button);
-
-            button.Click();
+                .Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.Id(buttonElementId))).Click();
         }
 
         protected void ClickButtonByXPath(string buttonElementPath)
