7bc03e430637e1190e966b3559eb2f091550c1d8
Jamie Kerr <60973239+jamiestephenkerr@users.noreply.github.com>
2024-04-24 11:47:03 +0100

    Blais5 4164 (#143)
    * Updated to NOT throw error but log when erroneous

--- Blaise.Tests.Helpers/Instrument/InstrumentHelper.cs
+++ Blaise.Tests.Helpers/Instrument/InstrumentHelper.cs
@@ -38,14 +38,15 @@ namespace Blaise.Tests.Helpers.Instrument
 
                 if (questionnaireStatus == QuestionnaireStatusType.Erroneous)
                 {
-                    throw new Exception($"ERROR: The {instrumentName} questionnaire has failed with the following status: {Enum.GetName(typeof(QuestionnaireStatusType), QuestionnaireStatusType.Erroneous)}. Blaise has probably got a lock on the questionnaire files and the Blaise service will likely need to be restarted on the Blaise management VM.");
+                    Console.WriteLine($"InstrumentHelper CheckIfInstrumentIsErroneous :Questionnaire {instrumentName} SEEMS to be erroneous. Check on the management node if the tests continue to fail");
+                    //throw new Exception($"ERROR: The {instrumentName} questionnaire has failed with the following status: {Enum.GetName(typeof(QuestionnaireStatusType), QuestionnaireStatusType.Erroneous)}. Blaise has probably got a lock on the questionnaire files and the Blaise service will likely need to be restarted on the Blaise management VM.");
                 }
 
-                Console.WriteLine($"InstrumentHelper CheckIfInstrumentIsErroneous :Questionnaire {BlaiseConfigurationHelper.InstrumentName} is not erroneous, it is in the state {questionnaireStatus}");
+                Console.WriteLine($"InstrumentHelper CheckIfInstrumentIsErroneous :Questionnaire {instrumentName} is not erroneous, it is in the state {questionnaireStatus}");
             }
             catch (DataNotFoundException)
             {
-                Console.WriteLine($"InstrumentHelper CheckIfInstrumentIsErroneous :Questionnaire {BlaiseConfigurationHelper.InstrumentName} does not exist");
+                Console.WriteLine($"InstrumentHelper CheckIfInstrumentIsErroneous :Questionnaire {instrumentName} does not exist");
             }
         }
 
