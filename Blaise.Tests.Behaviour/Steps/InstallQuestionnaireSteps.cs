d1298add7d93151715040de77c61572f74b63e14
Richmond Rice <richmond@ricey.org>
2024-08-07 09:40:41 +0100

    try to stop test early if erroneous

--- Blaise.Tests.Behaviour/Steps/InstallQuestionnaireSteps.cs
+++ Blaise.Tests.Behaviour/Steps/InstallQuestionnaireSteps.cs
@@ -11,7 +11,12 @@ namespace Blaise.Tests.Behaviour.Steps
     [Binding]
     public sealed class InstallQuestionnaireSteps
     {
-        // For additional details on SpecFlow step definitions see https://go.specflow.org/doc-stepdef
+        private readonly QuestionnaireHelper _questionnaireHelper;
+
+        public InstallQuestionnaireSteps()
+        {
+            _questionnaireHelper = QuestionnaireHelper.GetInstance();
+        }
 
         [Given(@"I have an questionnaire I want to use to capture respondents data")]
         public void GivenIHaveAQuestionnaireIWantToUseToCaptureRespondentsData()
@@ -30,48 +35,33 @@ namespace Blaise.Tests.Behaviour.Steps
         [When(@"I install the questionnaire into a Blaise environment specifying a Cati configuration")]
         public void WhenIInstallTheQuestionnaireIntoABlaiseEnvironment()
         {
-            var questionnaireHelper = QuestionnaireHelper.GetInstance();
-            var questionnaireStatus = questionnaireHelper.GetQuestionnaireStatus();
-
-            if (questionnaireStatus == QuestionnaireStatusType.Erroneous)
-            {
-                Assert.Fail("The questionnaire is in an erroneous state, please restart Blaise on the management VM and uninstall it via Blaise Server Manager");
-            }
-
-            questionnaireHelper.InstallQuestionnaire();
-
-            questionnaireStatus = questionnaireHelper.GetQuestionnaireStatus();
-
-            if (questionnaireStatus == QuestionnaireStatusType.Erroneous)
-            {
-                Assert.Fail("The questionnaire is in an erroneous state, please restart Blaise on the management VM and uninstall it via Blaise Server Manager");
-            }
+            _questionnaireHelper.CheckIfQuestionnaireIsErroneous(BlaiseConfigurationHelper.QuestionnaireName);
+            _questionnaireHelper.InstallQuestionnaire();
+            _questionnaireHelper.CheckIfQuestionnaireIsErroneous(BlaiseConfigurationHelper.QuestionnaireName);
         }
 
         [Then(@"the questionnaire is available to use in the Blaise environment")]
         public void ThenTheQuestionnaireIsAvailableToUseInTheBlaiseEnvironment()
         {
-            var questionnaireHasInstalled = QuestionnaireHelper.GetInstance().SurveyHasInstalled(BlaiseConfigurationHelper.QuestionnaireName, 60);
-
+            var questionnaireHasInstalled = _questionnaireHelper.SurveyHasInstalled(BlaiseConfigurationHelper.QuestionnaireName, 60);
             Assert.IsTrue(questionnaireHasInstalled, "The questionnaire has not been installed, or is not active");
         }
 
         [Then(@"the questionnaire is configured to capture respondents data via Cati")]
         public void ThenTheQuestionnaireIsConfiguredToCaptureRespondentsDataViaCati()
         {
-            var surveyConfiguration = QuestionnaireHelper.GetInstance().GetSurveyInterviewType();
+            var surveyConfiguration = _questionnaireHelper.GetSurveyInterviewType();
             Assert.AreEqual(QuestionnaireInterviewType.Cati, surveyConfiguration);
         }
 
         [AfterScenario("questionnaire")]
         public void CleanUpScenario()
         {
-            var questionnaireHelper = QuestionnaireHelper.GetInstance();
-            var questionnaireStatus = questionnaireHelper.GetQuestionnaireStatus();
+            var questionnaireStatus = _questionnaireHelper.GetQuestionnaireStatus();
 
             if (questionnaireStatus != QuestionnaireStatusType.Erroneous)
             {
-                questionnaireHelper.UninstallSurvey();
+                _questionnaireHelper.UninstallSurvey();
             }
             else
             {
