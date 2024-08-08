65d85a6d8135e75cb51077936794008df495cf31
Richmond Rice <richmond@ricey.org>
2024-08-08 11:24:40 +0100

    try to stop test early if erroneous

--- Blaise.Tests.Behaviour/Steps/CommonHooks.cs
+++ Blaise.Tests.Behaviour/Steps/CommonHooks.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using Blaise.Tests.Helpers.Browser;
+using Blaise.Tests.Helpers.Questionnaire;
+using Blaise.Tests.Helpers.Configuration;
+using Blaise.Nuget.Api.Contracts.Enums;
 using NUnit.Framework;
 using TechTalk.SpecFlow;
 
@@ -9,10 +12,35 @@ namespace Blaise.Tests.Behaviour.Steps
     public sealed class CommonHooks
     {
         private readonly ScenarioContext _scenarioContext;
+        private readonly QuestionnaireHelper _questionnaireHelper;
+
+        private const string ErroneousQuestionnaireAscii = @"
+                 _____                                            _ 
+                |  ___|                                          | |
+                | |__ _ __ _ __ ___  _ __   ___  ___  _   _ ___  | |
+                |  __| '__| '__/ _ \| '_ \ / _ \/ _ \| | | / __| | |
+                | |__| |  | | | (_) | | | |  __/ (_) | |_| \__ \ |_|
+                \____/_|  |_|  \___/|_| |_|\___|\___/ \__,_|___/ (_)
+                ";
+        private static readonly string ErroneousQuestionnaireMessage =
+        $"The test questionnaire {BlaiseConfigurationHelper.QuestionnaireName} is in an erroneous state.\n" +
+        "Restart Blaise and uninstall the erroneous questionnaire via Blaise Server Manager.";
 
         public CommonHooks(ScenarioContext scenarioContext)
         {
             _scenarioContext = scenarioContext;
+        }
+
+        [OneTimeSetUp]
+        public void CheckQuestionnaireStatusBeforeTestRun()
+        {
+            CheckQuestionnaireStatus(_questionnaireHelper);
+        }
+
+        [BeforeScenario(Order = -1)]
+        public void CheckQuestionnaireStatusBeforeScenario()
+        {
+            CheckQuestionnaireStatus(_questionnaireHelper);
         }
 
         [AfterStep]
@@ -22,6 +50,16 @@ namespace Blaise.Tests.Behaviour.Steps
             {
                 BrowserHelper.OnError(TestContext.CurrentContext, _scenarioContext);
                 throw new Exception(_scenarioContext.TestError.Message);
+            }
+        }
+
+        private static void CheckQuestionnaireStatus(QuestionnaireHelper questionnaireHelper)
+        {
+            var questionnaireStatus = questionnaireHelper.GetQuestionnaireStatus();
+
+            if (questionnaireStatus == QuestionnaireStatusType.Erroneous)
+            {
+                throw new InvalidOperationException($"{ErroneousQuestionnaireAscii}\n{ErroneousQuestionnaireMessage}");
             }
         }
     }
