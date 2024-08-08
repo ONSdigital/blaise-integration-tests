32866e6c3f27641455733f058e45001cb70e59bd
Richmond Rice <richmond@ricey.org>
2024-08-08 19:34:25 +0100

    try to stop test early if erroneous

--- Blaise.Tests.Behaviour/Steps/CommonHooks.cs
+++ Blaise.Tests.Behaviour/Steps/CommonHooks.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using Blaise.Nuget.Api.Contracts.Enums;
+﻿﻿using System;
 using Blaise.Tests.Helpers.Browser;
-using Blaise.Tests.Helpers.Configuration;
-using Blaise.Tests.Helpers.Questionnaire;
 using NUnit.Framework;
 using TechTalk.SpecFlow;
 
@@ -12,36 +9,10 @@ namespace Blaise.Tests.Behaviour.Steps
     public sealed class CommonHooks
     {
         private readonly ScenarioContext _scenarioContext;
-        private readonly QuestionnaireHelper _questionnaireHelper;
-
-        private const string ErroneousQuestionnaireAscii = @"
-                 _____                                            _ 
-                |  ___|                                          | |
-                | |__ _ __ _ __ ___  _ __   ___  ___  _   _ ___  | |
-                |  __| '__| '__/ _ \| '_ \ / _ \/ _ \| | | / __| | |
-                | |__| |  | | | (_) | | | |  __/ (_) | |_| \__ \ |_|
-                \____/_|  |_|  \___/|_| |_|\___|\___/ \__,_|___/ (_)
-                ";
-        private static readonly string ErroneousQuestionnaireMessage =
-        $"The test questionnaire {BlaiseConfigurationHelper.QuestionnaireName} is in an erroneous state.\n" +
-        "Restart Blaise and uninstall the erroneous questionnaire via Blaise Server Manager.";
 
         public CommonHooks(ScenarioContext scenarioContext)
         {
             _scenarioContext = scenarioContext;
-            _questionnaireHelper = new QuestionnaireHelper();
-        }
-
-        [OneTimeSetUp]
-        public void CheckQuestionnaireStatusBeforeTestRun()
-        {
-            CheckQuestionnaireStatus(_questionnaireHelper, _scenarioContext);
-        }
-
-        [BeforeScenario(Order = -1)]
-        public void CheckQuestionnaireStatusBeforeScenario()
-        {
-            CheckQuestionnaireStatus(_questionnaireHelper, _scenarioContext);
         }
 
         [AfterStep]
@@ -50,32 +21,7 @@ namespace Blaise.Tests.Behaviour.Steps
             if (_scenarioContext.TestError != null)
             {
                 BrowserHelper.OnError(TestContext.CurrentContext, _scenarioContext);
-                throw _scenarioContext.TestError;
-            }
-        }
-
-        private void CheckQuestionnaireStatus(QuestionnaireHelper questionnaireHelper, ScenarioContext scenarioContext)
-        {
-            try
-            {
-                var questionnaireStatus = questionnaireHelper.GetQuestionnaireStatus();
-
-                if (questionnaireStatus == QuestionnaireStatusType.Erroneous)
-                {
-                    throw new InvalidOperationException($"{ErroneousQuestionnaireAscii}\n{ErroneousQuestionnaireMessage}");
-                }
-            }
-            catch (Exception ex)
-            {
-                if (ex.Message.Contains("No questionnaire found"))
-                {
-                    Console.WriteLine($"Questionnaire {BlaiseConfigurationHelper.QuestionnaireName} not found, continuing with the tests.");
-                }
-                else
-                {
-                    Console.WriteLine($"An error occurred while checking the questionnaire status: {ex.Message}");
-                    throw;
-                }
+                throw new Exception(_scenarioContext.TestError.Message);
             }
         }
     }
