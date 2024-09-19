c7f994ad7d47491fcc812a61cbca3f1abd11c655
lambeb <141648830+lambeb@users.noreply.github.com>
2024-09-19 16:51:24 +0100

    Adding BENNY to log as can't find it in logs as too much outputs

--- Blaise.Tests.Helpers/Framework/Extensions/ConfigurationExtensions.cs
+++ Blaise.Tests.Helpers/Framework/Extensions/ConfigurationExtensions.cs
@@ -8,6 +8,7 @@ namespace Blaise.Tests.Helpers.Framework.Extensions
         public static string GetEnvironmentVariable(string variableName)
         {
             var variable = Environment.GetEnvironmentVariable(variableName) ?? GetVariable(variableName);
+            Console.WriteLine($"BENNY1 {variableName} = {variable}");
             variable.ThrowExceptionIfNullOrEmpty(variableName);
             return variable;
         }
@@ -15,7 +16,7 @@ namespace Blaise.Tests.Helpers.Framework.Extensions
         public static string GetVariable(string variableName)
         {
             var variable = ConfigurationManager.AppSettings[variableName];
-            Console.WriteLine($"{variableName} = {variable}");
+            Console.WriteLine($"BENNY2 {variableName} = {variable}");
             variable.ThrowExceptionIfNullOrEmpty(variableName);
             return variable;    
         }
@@ -23,6 +24,7 @@ namespace Blaise.Tests.Helpers.Framework.Extensions
         public static int GetIntVariable(string variableName)
         {
             var variable = ConfigurationManager.AppSettings[variableName];
+            Console.WriteLine($"BENNY3 {variableName} = {variable}");
             variable.ThrowExceptionIfNotInt(variableName);
             return Convert.ToInt32(variable);
         }
