74ef9e240520beb15b3e2b8c3fc9b063bf113979
sharaz15 <shahraz15@gmail.com>
2026-01-12 12:10:03 +0000

    WIP added logic for survey page, shifted from oo principiles in the interest of readability - have contained everything in one class clearly showing old and new attributes side by side

--- Blaise.Tests.Helpers/Cati/Pages/SurveyPage.cs
+++ Blaise.Tests.Helpers/Cati/Pages/SurveyPage.cs
@@ -1,65 +1,150 @@
-using Blaise.Tests.Helpers.Configuration;
-using Blaise.Tests.Helpers.Framework;
-using OpenQA.Selenium;
 using System;
 using System.Configuration;
 using System.Threading;
+using Blaise.Tests.Helpers.Configuration;
+using Blaise.Tests.Helpers.Framework;
+using OpenQA.Selenium;
 
-public class SurveyPage : BasePage
+namespace Blaise.Tests.Helpers.Cati.Pages
 {
-    private readonly ISurveyPageSelectors _selectors;
-
-    public SurveyPage()
-        : base(CatiConfigurationHelper.SurveyUrl)
+    public class SurveyPage : BasePage
     {
-        var isNewVersion = Convert.ToBoolean(ConfigurationManager.AppSettings["ENV_IS_BLAIS16"]);
+        private readonly string _blaiseVersion =
+            ConfigurationManager.AppSettings["ENV_BLAISE_VERSION"];
 
-        if (isNewVersion)
+        private const string FilterButtonPath = "//*[contains(text(), 'Filters')]";
+        private const string ApplyButtonPath = "//*[contains(text(), 'Apply')]";
+        private readonly string _surveyRadioButton =
+            $"//*[normalize-space()='{BlaiseConfigurationHelper.QuestionnaireName}']";
+
+        public SurveyPage()
+            : base(CatiConfigurationHelper.SurveyUrl)
         {
-            _selectors = new NewSurveyPageSelectors();
         }
-        else
+
+        #region Versioned selectors
+
+        private string ClearCatiDataButtonPath
         {
-            _selectors = new SurveyPageSelectors();
+            get
+            {
+                switch (_blaiseVersion?.ToLowerInvariant())
+                {
+                    case "v14":
+                        return "//*[@id='MVCGridTable_SurveysGrid']/tbody/tr/td[9]/a";
+
+                    case "v16":
+                        return "//*[contains(text(), 'Clear')]";
+
+                    default:
+                        throw new ConfigurationErrorsException(
+                            $"Unsupported ENV_BLAISE_VERSION value: '{_blaiseVersion}'. Expected 'v14' or 'v16'.");
+                }
+            }
         }
-    }
 
-    protected override Func<IWebDriver, bool> PageHasLoaded()
-    {
-        return BodyContainsText(_selectors.ExpectedPageText);
-    }
-
-    public void ClearDayBatchEntries()
-    {
-        Thread.Sleep(2000);
-
-        ClickButtonByXPath(_selectors.ClearCatiDataButtonPath);
-        ClickButtonByIdOrXPath(_selectors.BackupDataButtonId);
-        ClickButtonByIdOrXPath(_selectors.ClearDataButton);
-        ClickButtonByXPath(_selectors.ExecuteButtonPath);
-    }
-
-    private void ClickButtonByIdOrXPath(string selector)
-    {
-        if (selector.StartsWith("//"))
+        private string BackupDataButtonSelector
         {
-            ClickButtonByXPath(selector);
+            get
+            {
+                switch (_blaiseVersion?.ToLowerInvariant())
+                {
+                    case "v14":
+                        return "chkBackupAll"; // ID
+
+                    case "v16":
+                        return "qa_backupdata_0"; // ID
+
+                    default:
+                        throw new ConfigurationErrorsException(
+                            $"Unsupported ENV_BLAISE_VERSION value: '{_blaiseVersion}'. Expected 'v14' or 'v16'.");
+                }
+            }
         }
-        else
+
+        private string ClearDataButtonSelector
         {
-            ClickButtonById(selector);
+            get
+            {
+                switch (_blaiseVersion?.ToLowerInvariant())
+                {
+                    case "v14":
+                        return "chkClearAll"; // ID
+
+                    case "v16":
+                        return "//label[@for='qa_clear_all']"; // XPath
+
+                    default:
+                        throw new ConfigurationErrorsException(
+                            $"Unsupported ENV_BLAISE_VERSION value: '{_blaiseVersion}'. Expected 'v14' or 'v16'.");
+                }
+            }
         }
-    }
 
-    public void ApplyFilter()
-    {
-        ClickButtonByXPath(_selectors.FilterButton);
-        var filterButtonText = GetElementTextByPath(_selectors.FilterButton);
-
-        if (filterButtonText != "Filters (active)")
+        private string ExecuteButtonPath
         {
-            ClickButtonByXPath(_selectors.SurveyRadioButton);
-            ClickButtonByXPath(_selectors.ApplyButton);
+            get
+            {
+                switch (_blaiseVersion?.ToLowerInvariant())
+                {
+                    case "v14":
+                        return "//input[@value='Execute']";
+
+                    case "v16":
+                        return "//button[@id='qa_btn_submit']";
+
+                    default:
+                        throw new ConfigurationErrorsException(
+                            $"Unsupported ENV_BLAISE_VERSION value: '{_blaiseVersion}'. Expected 'v14' or 'v16'.");
+                }
+            }
+        }
+
+        #endregion
+
+        protected override Func<IWebDriver, bool> PageHasLoaded()
+        {
+            return _blaiseVersion?.ToLowerInvariant() switch
+            {
+                "v14" => BodyContainsText("Showing"),
+                "v16" => BodyContainsText("Surveys"),
+                _ => throw new ConfigurationErrorsException(
+                    $"Unsupported ENV_BLAISE_VERSION value: '{_blaiseVersion}'. Expected 'v14' or 'v16'.")
+            };
+        }
+
+        public void ClearDayBatchEntries()
+        {
+            Thread.Sleep(2000);
+
+            ClickButtonByXPath(ClearCatiDataButtonPath);
+
+            // Backup checkbox
+            ClickButtonById(BackupDataButtonSelector);
+
+            // Clear checkbox differs by selector type
+            if (_blaiseVersion?.ToLowerInvariant() == "v14")
+            {
+                ClickButtonById(ClearDataButtonSelector);
+            }
+            else
+            {
+                ClickButtonByXPath(ClearDataButtonSelector);
+            }
+
+            ClickButtonByXPath(ExecuteButtonPath);
+        }
+
+        public void ApplyFilter()
+        {
+            ClickButtonByXPath(FilterButtonPath);
+
+            var filterButtonText = GetElementTextByPath(FilterButtonPath);
+            if (filterButtonText != "Filters (active)")
+            {
+                ClickButtonByXPath(_surveyRadioButton);
+                ClickButtonByXPath(ApplyButtonPath);
+            }
         }
     }
 }
