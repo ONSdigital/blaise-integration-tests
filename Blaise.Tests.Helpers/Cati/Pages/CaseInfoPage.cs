b44338ebbc3ce27a4d1a390fa0f3b5bcdfc393b1
sharaz15 <shahraz15@gmail.com>
2026-01-14 13:28:26 +0000

    updated caseinfopage

--- Blaise.Tests.Helpers/Cati/Pages/CaseInfoPage.cs
+++ Blaise.Tests.Helpers/Cati/Pages/CaseInfoPage.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Configuration;
 using System.Threading;
 using Blaise.Tests.Helpers.Browser;
 using Blaise.Tests.Helpers.Configuration;
@@ -9,11 +10,73 @@ namespace Blaise.Tests.Helpers.Cati.Pages
 {
     public class CaseInfoPage : BasePage
     {
-        private const string _questionnaireCell = "//*[@id='CaseInfo_content_table']/tbody/tr[1]/td[1]";
-        private const string _caseIdCell = "//*[@id='CaseInfo_content_table']/tbody/tr[1]/td[2]";
-        private const string _playButton = "qa_startcase_0";
+        private readonly string _blaiseVersion =
+            ConfigurationManager.AppSettings["ENV_BLAISE_VERSION"];
+
+        private string QuestionnaireCellPath
+        {
+            get
+            {
+                switch (_blaiseVersion?.ToLowerInvariant())
+                {
+                    case "v14":
+                        return "//*[@id='MVCGridTable_CaseInfoGrid']/tbody/tr[1]/td[1]";
+
+                    case "v16":
+                        return "//*[@id='CaseInfo_content_table']/tbody/tr[1]/td[1]";
+
+                    default:
+                        throw new ConfigurationErrorsException(
+                            $"Unsupported ENV_BLAISE_VERSION value: '{_blaiseVersion}'. Expected 'v14' or 'v16'.");
+                }
+            }
+        }
+
+        private string CaseIdCellPath
+        {
+            get
+            {
+                switch (_blaiseVersion?.ToLowerInvariant())
+                {
+                    case "v14":
+                        return "//*[@id='MVCGridTable_CaseInfoGrid']/tbody/tr[1]/td[2]";
+
+                    case "v16":
+                        return "//*[@id='CaseInfo_content_table']/tbody/tr[1]/td[2]";
+
+                    default:
+                        throw new ConfigurationErrorsException(
+                            $"Unsupported ENV_BLAISE_VERSION value: '{_blaiseVersion}'. Expected 'v14' or 'v16'.");
+                }
+            }
+        }
+
+        private string PlayButtonSelector
+        {
+            get
+            {
+                switch (_blaiseVersion?.ToLowerInvariant())
+                {
+                    case "v14":
+                        return "//*[@id='MVCGridTable_CaseInfoGrid']/tbody/tr[1]/td[19]/a/span";
+
+                    case "v16":
+                        return "qa_startcase_0";
+
+                    default:
+                        throw new ConfigurationErrorsException(
+                            $"Unsupported ENV_BLAISE_VERSION value: '{_blaiseVersion}'. Expected 'v14' or 'v16'.");
+                }
+            }
+        }
+
+        private bool PlayButtonIsById =>
+            _blaiseVersion?.Equals("v16", StringComparison.OrdinalIgnoreCase) == true;
+
         private const string _filterButton = "//*[contains(text(), 'Filters')]";
-        private readonly string _surveyRadioButton = $"//*[normalize-space()='{BlaiseConfigurationHelper.QuestionnaireName}']";
+        private readonly string _surveyRadioButton =
+            $"//*[normalize-space()='{BlaiseConfigurationHelper.QuestionnaireName}']";
+
         private const string _applyButton = "//*[contains(text(), 'Apply')]";
 
         public CaseInfoPage()
@@ -40,22 +103,42 @@ namespace Blaise.Tests.Helpers.Cati.Pages
             while (!FirstCaseIsPlayable());
         }
 
-        /// <inheritdoc/>
         protected override Func<IWebDriver, bool> PageHasLoaded()
         {
-            return BodyContainsText("Case Info");
+            switch (_blaiseVersion?.ToLowerInvariant())
+            {
+                case "v14":
+                    return BodyContainsText("Showing");
+
+                case "v16":
+                    return BodyContainsText("Case Info");
+
+                default:
+                    throw new ConfigurationErrorsException(
+                        $"Unsupported ENV_BLAISE_VERSION value: '{_blaiseVersion}'. Expected 'v14' or 'v16'.");
+            }
         }
+
 
         public void ClickPlayButton()
         {
             var numberOfWindows = BrowserHelper.GetNumberOfWindows();
-
             var attempts = 0;
+
             while (BrowserHelper.GetNumberOfWindows() == numberOfWindows)
             {
-                ClickButtonById(_playButton);
+                if (PlayButtonIsById)
+                {
+                    ClickButtonById(PlayButtonSelector);
+                }
+                else
+                {
+                    ClickButtonByXPath(PlayButtonSelector);
+                }
+
                 Thread.Sleep(250);
                 attempts++;
+
                 if (attempts > 5)
                 {
                     throw new Exception("Timed out waiting for new window to open.");
@@ -67,6 +150,7 @@ namespace Blaise.Tests.Helpers.Cati.Pages
         {
             ClickButtonByXPath(_filterButton);
             var filterButtonText = GetElementTextByPath(_filterButton);
+
             if (filterButtonText != "Filters (active)")
             {
                 ClickButtonByXPath(_surveyRadioButton);
@@ -78,17 +162,19 @@ namespace Blaise.Tests.Helpers.Cati.Pages
 
         private void WaitUntilFirstCaseQuestionnaireIs(string questionnaire)
         {
-            WaitUntilElementByXPathContainsText(_questionnaireCell, questionnaire);
+            WaitUntilElementByXPathContainsText(QuestionnaireCellPath, questionnaire);
         }
 
         private void WaitUntilFirstCaseIs(string caseId)
         {
-            WaitUntilElementByXPathContainsText(_caseIdCell, caseId);
+            WaitUntilElementByXPathContainsText(CaseIdCellPath, caseId);
         }
 
         public bool FirstCaseIsPlayable()
         {
-            return ElementIsDisplayed(By.Id(_playButton));
+            return PlayButtonIsById
+                ? ElementIsDisplayed(By.Id(PlayButtonSelector))
+                : ElementIsDisplayed(By.XPath(PlayButtonSelector));
         }
     }
 }
