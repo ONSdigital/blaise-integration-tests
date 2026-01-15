9f63cf19cba643e207d12a77b70309927d026bf2
sharaz15 <shahraz15@gmail.com>
2026-01-15 16:59:10 +0000

    refactored scheduler page

--- Blaise.Tests.Helpers/Cati/Pages/SchedulerPage.cs
+++ Blaise.Tests.Helpers/Cati/Pages/SchedulerPage.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Configuration;
 using Blaise.Tests.Helpers.Configuration;
 using Blaise.Tests.Helpers.Framework;
 
@@ -5,13 +7,36 @@ namespace Blaise.Tests.Helpers.Cati.Pages
 {
     public class SchedulerPage : BasePage
     {
-        private readonly string _usernameTextBoxName = "name";
-        private readonly string _passwordTextBoxName = "password";
-        private readonly string _submitButtonPath = "//input[@type='submit']";
+        private readonly string _blaiseVersion =
+            ConfigurationManager.AppSettings["ENV_BLAISE_VERSION"];
+
+        private const string _usernameTextBoxName = "name";
+        private const string _passwordTextBoxName = "password";
+        private const string _submitButtonPath = "//input[@type='submit']";
 
         public SchedulerPage()
-            : base(CatiConfigurationHelper.SchedulerUrl)
+            : base(
+                CatiConfigurationHelper.SchedulerUrl,
+                GetLayoutParameter())
         {
+        }
+
+        private static string GetLayoutParameter()
+        {
+            var version = ConfigurationManager.AppSettings["ENV_BLAISE_VERSION"];
+
+            switch (version?.ToLowerInvariant())
+            {
+                case "v14":
+                    return "LayoutSet=CATI-Interviewer_Large";
+
+                case "v16":
+                    return null; // no layout parameter required
+
+                default:
+                    throw new ConfigurationErrorsException(
+                        $"Unsupported ENV_BLAISE_VERSION value: '{version}'. Expected 'v14' or 'v16'.");
+            }
         }
 
         public void LogIntoScheduler(string username, string password)
