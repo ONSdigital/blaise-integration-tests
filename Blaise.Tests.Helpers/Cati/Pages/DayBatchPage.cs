988dd8b7f5e90fde60afbbe0dd01bf7a4ca81998
Richmond Rice <richmond@ricey.org>
2026-02-12 11:06:10 +0000

    516 cati changes

--- Blaise.Tests.Helpers/Cati/Pages/DayBatchPage.cs
+++ Blaise.Tests.Helpers/Cati/Pages/DayBatchPage.cs
@@ -81,21 +81,26 @@ namespace Blaise.Tests.Helpers.Cati.Pages
 
         public void ApplyFilter()
         {
-            Thread.Sleep(5000);
-            ClickButtonByXPath(FilterButton);
-            var filterButtonText = GetElementTextByPath(FilterButton);
-            if (filterButtonText != "Filters (active)")
+            if (UseNewSelectors)
             {
-                if (UseNewSelectors)
-                {
-                    BrowserHelper.ClickByIdWithRetry("qa_filter_surveymultiple");
-                    BrowserHelper.ClickByXPathWithRetry(_surveyRadioButton);
-                }
-                else
+                ClickButtonByXPath("//div[@e-mappinguid='qa_instrumentid' and contains(@class, 'e-filtermenudiv')]");
+                var dropdownSelector = "//span[contains(@class, 'e-ddl') and .//input[@id='qa_instrumentnameidfilter']]";
+                ClickButtonByXPath(dropdownSelector);
+                var listOptionPath = $"//li[contains(@class, 'e-list-item') and text()='{BlaiseConfigurationHelper.QuestionnaireName}']";
+                ClickButtonByXPath(listOptionPath);
+                ClickButtonByXPath("//button[contains(@class, 'e-flmenu-okbtn') and text()='Filter']");
+                Thread.Sleep(1000);
+            }
+            else
+            {
+                ClickButtonByXPath(FilterButton);
+                var filterButtonText = GetElementTextByPath(FilterButton);
+                if (filterButtonText != "Filters (active)")
                 {
                     ClickButtonByXPath(_surveyRadioButton);
+                    ClickButtonByXPath(ApplyButton);
                 }
-                ClickButtonByXPath(ApplyButton);
+                ClickButtonByXPath(FilterButton);
             }
         }
 
