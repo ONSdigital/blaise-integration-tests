4d9196e924181cc2886a34854e7b204fd40d48df
Richmond Rice <richmond@ricey.org>
2026-05-28 22:09:20 +0100

    dqs updates

--- Blaise.Tests.Helpers/Dqs/Pages/LoginPage.cs
+++ Blaise.Tests.Helpers/Dqs/Pages/LoginPage.cs
@@ -1,5 +1,7 @@
 namespace Blaise.Tests.Helpers.Dqs.Pages
 {
+    using System;
+    using System.Linq;
     using Blaise.Tests.Helpers.Browser;
     using Blaise.Tests.Helpers.Configuration;
     using Blaise.Tests.Helpers.Framework;
@@ -7,10 +9,14 @@ namespace Blaise.Tests.Helpers.Dqs.Pages
 
     public class LoginPage : BasePage
     {
+        private readonly string _usernameTextBoxId = "username";
         private readonly string _usernameTextBoxName = "Username";
+        private readonly string _passwordTextBoxId = "password";
         private readonly string _passwordTextBoxName = "Password";
         private readonly string _submitButtonPath = "//button[@type='submit']";
-        private readonly string _signOutId = "signout-button";
+        private readonly string _signOutButtonId = "signout-button";
+        private readonly string _signOutButtonPath = "//header[contains(@class,'ons-header')]//button[.//span[normalize-space()='Sign out']]";
+        private readonly string _signOutButtonCss = "header.ons-header button.ons-btn--link";
 
         public LoginPage()
             : base(DqsConfigurationHelper.DqsUrl)
@@ -19,25 +25,83 @@ namespace Blaise.Tests.Helpers.Dqs.Pages
 
         public void LogIntoDqs(string username, string password)
         {
-            PopulateInputByName(_usernameTextBoxName, username);
-            PopulateInputByName(_passwordTextBoxName, password);
+            PopulateLoginInput(_usernameTextBoxId, _usernameTextBoxName, username);
+            PopulateLoginInput(_passwordTextBoxId, _passwordTextBoxName, password);
             ClickButtonByXPath(_submitButtonPath);
             WaitUntilLoggedIn();
         }
 
         public void LogoutOfDqs()
         {
-            ClickButtonById(_signOutId);
+            var signOutButton = BrowserHelper
+                .Wait("Timed out waiting for Sign out button")
+                .Until(FindSignOutButton);
+            signOutButton.Click();
         }
 
         public bool IsLogoutButtonVisible()
         {
-            return BrowserHelper.ElementExistsById(_signOutId);
+            try
+            {
+                BrowserHelper
+                    .Wait("Timed out waiting for Sign out button", TimeSpan.FromSeconds(5))
+                    .Until(driver => FindSignOutButton(driver) != null);
+                return true;
+            }
+            catch (WebDriverTimeoutException)
+            {
+                return false;
+            }
         }
 
         private void WaitUntilLoggedIn()
         {
-            ButtonIsAvailableById(_signOutId);
+            BrowserHelper
+                .Wait("Timed out waiting for Sign out button")
+                .Until(driver => FindSignOutButton(driver) != null);
+        }
+
+        private void PopulateLoginInput(string elementId, string elementName, string value)
+        {
+            var element = BrowserHelper
+                .Wait($"Timed out in PopulateLoginInput(\"{elementId}\", \"{elementName}\")")
+                .Until(driver =>
+                {
+                    var byId = driver.FindElements(By.Id(elementId))
+                        .FirstOrDefault(candidate => candidate.Displayed);
+                    if (byId != null)
+                    {
+                        return byId;
+                    }
+
+                    return driver.FindElements(By.Name(elementName))
+                        .FirstOrDefault(candidate => candidate.Displayed);
+                });
+            element.Clear();
+            element.SendKeys(value);
+        }
+
+        // Fallback to older id selectors when running against legacy DQS UI.
+        private IWebElement FindSignOutButton(IWebDriver driver)
+        {
+            var byId = driver.FindElements(By.Id(_signOutButtonId))
+                .FirstOrDefault(candidate => candidate.Displayed);
+            if (byId != null)
+            {
+                return byId;
+            }
+
+            var byXPath = driver.FindElements(By.XPath(_signOutButtonPath))
+                .FirstOrDefault(candidate => candidate.Displayed);
+            if (byXPath != null)
+            {
+                return byXPath;
+            }
+
+            return driver.FindElements(By.CssSelector(_signOutButtonCss))
+                .FirstOrDefault(candidate =>
+                    candidate.Displayed &&
+                    candidate.Text.IndexOf("Sign out", StringComparison.OrdinalIgnoreCase) >= 0);
         }
 
         protected override By PageIdentityBy => By.XPath(_submitButtonPath);
